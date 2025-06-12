e21b720dfec4c9c2179e1e60599558a3768afe91
Snehaagraw <agrawalsneha3432@gmail.com>
2025-06-12 21:42:12 +0530

    commit2

---
+++ Core-Java/Day1/Programs.java
@@ -0,0 +1,87 @@
+/* 1) public class Programs{
+	public static void main(String[] args){
+		System.out.println("Welcome to Bridgelabz!");
+	}
+}*/
+/* 2) import java.util.*;
+public class Programs{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int add=n+m;
+		System.out.println(add);
+	}
+}*/
+//3)
+ /*import java.util.*;
+public class Programs{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int celsius=sc.nextInt();
+		int fahrenheit;
+		fahrenheit = ((celsius * 9/5) + 32);
+		System.out.print(fahrenheit);
+	}
+}*/
+//4)
+/*import java.util.*;
+public class Programs{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		double radius=sc.nextInt();
+		double area;
+		area=3.14*radius*radius;
+		System.out.println("Area of circle:" + area);
+	}
+}*/
+//5)
+/*import java.util.*;
+public class Programs{
+	public static void  main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		float radius=sc.nextInt();
+		float height=sc.nextInt();
+		float pie=(float) 3.14;
+		float volume=pie*radius*radius*height;
+		System.out.println("volume:"+ volume);
+
+	}
+}*/
+//6)
+/*import java.util.*;
+public class Programs{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int principal=sc.nextInt();
+		int rate=sc.nextInt();
+		int time=sc.nextInt();
+		int interset=((principal*rate*time)/100);
+		System.out.println("simple interest:"+ interset);
+	}
+}*/
+//7)
+/*import java.util.*;
+public class Programs{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int length=sc.nextInt();
+		int width=sc.nextInt();
+		int perimeter= (2*(length+width));
+		System.out.println("perimeter:" + perimeter);
+	}
+}*/
+//8)
+import java.util.*;
+public class Programs{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int base=sc.nextInt();
+		int exponent=sc.nextInt();
+		int cube=Math.pow(base , exponent);
+		System.out.println(cubes);
+	}
+
+}
+
+
